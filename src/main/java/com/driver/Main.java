652cdb2395cd36b6395610b4bb12e32eadd5ddc3
tusharbhendarkar30 <121106268+tusharbhendarkar30@users.noreply.github.com>
2024-09-02 10:36:52 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class Main {
+  public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        obj.name = "John"; 
+    // Error: 'name' has private access in 'RWOnly'
+        System.out.println(obj.name);
+    //we cannot access private members outside the class
+    //*********************************************************************************    
+    //by using getter setter method we can access private members outside the class
+        obj.setName("John");
+        
+        
+        System.out.println(obj.getName());
   
 }
